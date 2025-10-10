4d32ee3230fd8f74875952dfd7d083fb555fbf5e
gigaxucishvili2007 <158300399+gigaxucishvili2007@users.noreply.github.com>
2025-10-10 19:13:46 +0400

    IT

--- Level 029/homework/main9.py
+++ Level 029/homework/main9.py
@@ -3,12 +3,12 @@
 #            0         1         2        3        4          5          6
 animals = ["Lion" , "Tiger" , "Frog" , "Panda" , "Deer" , "Elephant" , "Fox"]
 #           -7        -6       -5        -4       -3         -2         -1
-print(animals[1])
-print(animals[2])
-print(animals[4])
-print(animals[6])
+print(animals[1]) # Tiger
+print(animals[2]) # Frog
+print(animals[4]) # Deer
+print(animals[6]) # Fox
 
-print(animals[-2])
-print(animals[-3])
-print(animals[-5])
-print(animals[-7])
+print(animals[-2]) # Elephant
+print(animals[-3]) # Deer
+print(animals[-5]) # Frog
+print(animals[-7]) # Lion
